be0a88e07d22c8932b1804cc5cacedc7ab0658bb
Jacob Kostecki <kostecki.jacob@gmail.com>
2018-06-09 18:06:32 +0200

    working post requests with color info

--- src/components/Lights/SolidColors/SolidColors.js
+++ src/components/Lights/SolidColors/SolidColors.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import Paper from '@material-ui/core/Paper';
-import Icon from '@material-ui/core/Icon';
 import Divider from '@material-ui/core/Divider';
 import { HuePicker, AlphaPicker } from 'react-color';
 
@@ -16,16 +16,19 @@ class SolidColors extends Component {
       g: 0,
       b: 0,
       a: 1
-    }
+    },
+    brightness: {
+      a: 0.5
+    },
+    animation: null
   }
 
   handleHueChangeComplete = (color) => {
-    this.setState({color: color.rgb});
-    console.log(color.rgb);
+    this.setState({color: color.rgb}, () => this.postColorChange());
   }
 
   handleAlphaChangeComplete = (color) => {
-    this.setState({color: color.rgb});
+    this.setState({color: color.rgb}, () => this.postColorChange());
   }
 
   handlePresetClick = (color) => {
@@ -35,21 +38,38 @@ class SolidColors extends Component {
       b: parseInt(color.substring(5,7),16),
       a: 1
     }
-    this.setState({color: newColor});
+    this.setState({color: newColor}, () => this.postColorChange());
   }
 
-  rgbaString = (color) => {
+  handleBrightnessChangeComplete = (color) => {
+    this.setState({brightness: color.rgb}, () => this.postColorChange());
+  }
+
+  createRgbaString = (color) => {
     return `rgba(${this.state.color.r}, ${this.state.color.g}, ${this.state.color.b}, ${this.state.color.a})`;
+  }
+
+  postColorChange = () => {
+    console.log("Posting..");
+    axios.post('https://putsreq.com/13pmCr7IuFD50z8RzzOz', {
+      payload: this.state
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
   }
 
   render() {
     return (
       <Card className={styles.card}>
-        <h3 className={styles.cardTitle}>Farver | {this.state.color.r}, {this.state.color.g}, {this.state.color.b}, {this.state.color.a}</h3>
+        <h3 className={styles.cardTitle}>Farver</h3>
         <Divider className={styles.divider} />
         <Grid container spacing={24}>
           <Grid item xs={4}>
-            <div className={styles.colorPreview} style={{background: this.rgbaString(this.state.color)}}></div>
+            <div className={styles.colorPreview} style={{background: this.createRgbaString(this.state.color)}}></div>
           </Grid>
           <Grid item xs={8} className={styles.colorPresets}>
             <Paper className={styles.colorPreset} style={{ background: '#ff0000' }} onClick={() => {this.handlePresetClick('#ff0000')}}></Paper>
@@ -65,12 +85,18 @@ class SolidColors extends Component {
 
         <Grid container spacing={24} className={styles.sliders}>
           <Grid item xs={6} className={styles.brightnessSlider}>
-            <p>Lysstyrke</p>
+            <p>Lysstyrke ({Math.trunc(this.state.brightness.a * 100)}%)</p>
             <AlphaPicker 
-              className={styles.alphaPicker} />
+              className={styles.alphaPicker}
+              color={this.state.brightness}
+              onChangeComplete={this.handleBrightnessChangeComplete} />
+            <div className={styles.brightnessIcons}>
+              <i className="far fa-lightbulb"></i>
+              <i className="fas fa-lightbulb"></i>
+            </div>
           </Grid>
           <Grid item xs={6}>
-            <p>Custom Color</p>
+            <p>Farvælger ({this.state.color.r}, {this.state.color.g}, {this.state.color.b}, {this.state.color.a})</p>
             <HuePicker
               className={styles.huePicker} 
               color={this.state.color} 
