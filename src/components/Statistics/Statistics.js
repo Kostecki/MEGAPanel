6b4818b9be85aeec65a06f78a1ad1a17e8cd2839
Jacob Kostecki <kostecki.jacob@gmail.com>
2018-06-10 18:10:09 +0200

    i.. wrote.. things. Maybe statisics

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import CardDefault from './../UI/CardDefault/CardDefault';
+import UsageIntervals from './UsageIntervals/UsageIntervals';
+
+import styles from './Statistics.module.css';
+
 const Statistics = () => (
-  <h1>Statistics</h1>
+  <div className={styles.statsContainer}>
+    <CardDefault title='Statistik'>
+      <UsageIntervals />
+    </CardDefault>
+  </div>
 )
 
 export default Statistics;
---
+++ src/components/Statistics/UsageIntervals/UsageInterval/UsageInterval.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import { defaults, Doughnut } from 'react-chartjs-2';
+import Moment from 'moment';
+
+import styles from './UsageInterval.module.css';
+
+const UsageInterval = (props) => {
+  defaults.global.legend.display = false;
+
+  const usageIntervalPeriod = (period) => {
+    let now = Moment();
+    let range = '';
+    let startOfWeek = Moment().startOf('isoweek').format('D.');
+    let endOfWeek = Moment().endOf('isoweek').format('D. MMMM');
+
+    switch (period) {
+      case 'Time':
+        range = now.format('HH:MM');
+        break;
+      
+      case 'Dag':
+        range = now.format('dddd, D. MMMM');
+        break;
+
+      case 'Uge':
+        range = startOfWeek + '-' + endOfWeek;
+        break;
+    
+      default:
+        break;
+    }
+
+    return range;
+  }
+
+  const data = {
+    labels: [
+      'Upload',
+      'Download'
+    ],
+    datasets: [{
+      data: [props.upload, props.download],
+      backgroundColor: [
+        '#616774',
+        '#46BFBD'
+      ],
+      hoverBackgroundColor: [
+        '#616774',
+        '#5AD3D1'
+      ]
+    }]
+  };
+
+  const options = {
+    rotation: 1 * Math.PI,
+    circumference: 1 * Math.PI,
+    cutoutPercentage: 80,
+    tooltips: {
+      callbacks: {
+        label: function (tooltipItems, data) {
+          return data.labels[tooltipItems.datasetIndex] + ': ' + data.datasets[tooltipItems.datasetIndex].data[tooltipItems.index] + props.unit.toUpperCase();
+        }
+      }
+    }
+  }
+  return (
+    <Card className={`${styles.usageInterval} ${props.period.toLowerCase()}`}>
+      <p>{props.period} ({usageIntervalPeriod(props.period)})</p>
+      <Grid container spacing={16}>
+        <Grid item xs={12}>
+          <p className={styles.textInsideDoughnut}><i className="fas fa-long-arrow-alt-up"></i> {`${props.upload}/${props.download}`} <i className="fas fa-long-arrow-alt-down"></i></p>
+          <Doughnut data={data} options={options} />
+        </Grid>
+      </Grid>
+    </Card>
+  )
+}
+
+export default UsageInterval;
