fad8e0eb5216d024ac4b20a8c2f49a03e51b49ed
Jacob Kostecki <kostecki.jacob@gmail.com>
2018-06-10 22:41:18 +0200

    add active state to bottom nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {withRouter} from 'react-router-dom'
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
@@ -8,19 +8,40 @@ import InsertChartOutlined from '@material-ui/icons/InsertChart';
 
 import styles from './Navigation.module.css';
 
-const Navigation = (props) => {
-  let handleChange = (value, event) => {
-    switch (event) {
+class Navigation extends Component {
+  state = {
+    value: null
+  }
+
+  componentDidMount() {
+    //I'm sorry. This is gross..
+    let curPage = this.props.location.pathname;
+    switch(curPage) {
+      case '/lights':
+        this.setState({value: 1})
+        break;
+      
+      case '/stats':
+        this.setState({value: 2})
+        break;
+      
+      default:
+        break;
+    }
+  }
+
+  handleChange = (event, value) => {
+    switch (value) {
       case 0:
-        props.history.push('/')
+        this.props.history.push('/')
         break;
 
       case 1:
-        props.history.push('/lights')
+        this.props.history.push('/lights')
         break;
 
       case 2:
-        props.history.push('/stats')
+        this.props.history.push('/stats')
         break;
     
       default:
@@ -28,17 +49,20 @@ const Navigation = (props) => {
     }
   }
 
-  return (
-    <BottomNavigation
-      onChange={handleChange}
-      showLabels
-      className={styles.bottomNav}
-    >
-      <BottomNavigationAction label="Hjem" icon={<HomeIcon />} />
-      <BottomNavigationAction label="Lys" icon={<ColorLensIcon />} />
-      <BottomNavigationAction label="Statistik" icon={<InsertChartOutlined />} />
-    </BottomNavigation>
-  )
+  render() {  
+    return (
+      <BottomNavigation
+        onChange={this.handleChange}
+        showLabels
+        className={styles.bottomNav}
+        value={this.state.value}
+      >
+        <BottomNavigationAction label="Hjem" icon={<HomeIcon />} />
+        <BottomNavigationAction label="Lys" icon={<ColorLensIcon />} />
+        <BottomNavigationAction label="Statistik" icon={<InsertChartOutlined />} />
+      </BottomNavigation>
+    )
+  }
 }
 
 export default withRouter(Navigation);
