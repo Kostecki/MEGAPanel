104f99b4462af49e1d31c16012452a744fba465b
Jacob Kostecki <kostecki.jacob@gmail.com>
2018-06-13 19:41:52 +0200

    Committed some changes..

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import ReactGA from 'react-ga';
 
@@ -10,16 +10,21 @@ import './App.css';
 
 ReactGA.initialize('UA-39781810-11');
 
-const App = (props) => {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path='/' component={Landing} />
-        <Route path='/lights' component={Lights} />
-        <Route path='/stats' component={Stats} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  componentDidMount  = () => ReactGA.pageview(window.location.pathname + window.location.search);
+  componentDidUpdate = () => ReactGA.pageview(window.location.pathname + window.location.search);
+
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route path='/lights' component={Lights} />
+          <Route path='/stats' component={Stats} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
