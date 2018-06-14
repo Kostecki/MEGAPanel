5055f0ab29b1ef06d7af27c297a78aee7f78253f
Jacob Kostecki <kostecki.jacob@gmail.com>
2018-06-14 15:00:42 +0200

    VoltageGraph component

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Grid from '@material-ui/core/Grid';
 
 import CardDefault from './../UI/CardDefault/CardDefault';
@@ -6,16 +7,18 @@ import AvgSpeed from './AvgSpeed/AvgSpeed';
 import BatteryVoltage from './BatteryVoltage/BatteryVoltage';
 import UsageInterval from './UsageInterval/UsageInterval';
 import DailyBarGraph from './DailyBarGraph/DailyBarGraph';
+import VoltageGraph from './VoltageGraph/VoltageGraph';
 import Navigation from '../Navigation/Navigation';
 
 import styles from './Statistics.module.css';
 
 const Statistics = () => (
+
   <React.Fragment>
     <div className={styles.statsContainer}>
       <CardDefault title='Statistik'>
         <Grid item xs={12} className={styles.group1}>
-          <BatteryVoltage voltage='12,7' />
+          <BatteryVoltage voltage={'12,7'} />
           <AvgSpeed download='314 kbit/s' upload='129 kbit/s' />
         </Grid>
         <Grid item xs={12} className={styles.group2}>
@@ -25,6 +28,9 @@ const Statistics = () => (
         </Grid>
         <Grid item xs={12} className={styles.group3}>
           <DailyBarGraph />
+        </Grid>
+        <Grid item xs={12}>
+          <VoltageGraph />
         </Grid>
       </CardDefault>
     </div>
---
+++ src/components/Statistics/VoltageGraph/VoltageGraph.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import { Line } from 'react-chartjs-2';
+
+import styles from './VoltageGraph.module.css';
+
+const data = {
+  datasets: [{
+    data: ['11', '11', '12', '12', '12', '12', '12', '11', '13', '13', '11', '11', '12', '13', '12', '12', '12', '11', '11', '12', '11', '12', '11', '12', '12', '11', '11', '12', '12', '12', '12', '12', '11', '13', '13', '11', '11', '12', '13', '12', '12', '12'],
+    label: 'Spænding',
+    yAxisID: 'left-y-axis'
+  }],
+  labels: [
+    'Mandag: 04:00',
+    'Mandag: 08:00',
+    'Mandag: 12:00',
+    'Mandag: 16:00',
+    'Mandag: 20:00',
+    'Mandag: 00:00',
+    'Tirsdag: 04:00',
+    'Tirsdag: 08:00',
+    'Tirsdag: 12:00',
+    'Tirsdag: 16:00',
+    'Tirsdag: 20:00',
+    'Tirsdag: 00:00',
+    'Onsdag: 04:00',
+    'Onsdag: 08:00',
+    'Onsdag: 12:00',
+    'Onsdag: 16:00',
+    'Onsdag: 20:00',
+    'Onsdag: 00:00',
+    'Torsdag: 04:00',
+    'Torsdag: 08:00',
+    'Torsdag: 12:00',
+    'Torsdag: 16:00',
+    'Torsdag: 20:00',
+    'Torsdag: 00:00',
+    'Fredag: 04:00',
+    'Fredag: 08:00',
+    'Fredag: 12:00',
+    'Fredag: 16:00',
+    'Fredag: 20:00',
+    'Fredag: 00:00',
+    'Lørdag: 04:00',
+    'Lørdag: 08:00',
+    'Lørdag: 12:00',
+    'Lørdag: 16:00',
+    'Lørdag: 20:00',
+    'Lørdag: 00:00',
+    'Søndag: 04:00',
+    'Søndag: 08:00',
+    'Søndag: 12:00',
+    'Søndag: 16:00',
+    'Søndag: 20:00',
+    'Søndag: 00:00'
+  ]
+}
+const options = {
+  scales: {
+    yAxes: [{
+      id: 'left-y-axis',
+      type: 'linear',
+      position: 'left'
+    }]
+  }
+}
+
+const VoltageGraph = () => (
+  <Card className={styles.voltageGraph}>
+    <p>Batterierne i løbet af ugen</p>
+    <Line data={data} options={options} />
+  </Card>
+)
+
+export default VoltageGraph;
