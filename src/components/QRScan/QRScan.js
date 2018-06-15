0ef6db378623af65e1f5f4cc285d37cc29a33afa
Jacob Kostecki <kostecki.jacob@gmail.com>
2018-06-15 10:42:21 +0200

    add qr scanning

--- src/components/QRScan/QRScan.js
+++ src/components/QRScan/QRScan.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
+import QrReader from 'react-qr-reader'
 import Grid from '@material-ui/core/Grid';
 
 import CardDefault from '../UI/CardDefault/CardDefault';
 import Navigation from '../Navigation/Navigation';
 
 class QRScan extends Component {
+  state = {
+    result: ''
+  }
+
+  handleCameraScan = (data) => {
+    if (data) {
+      this.setState({result: data})
+    }
+    console.log(data);
+  }
+
+  handleCameraError = (err) => {
+    console.log(err);
+  }
+
   render() {
     return (
       <React.Fragment>
         <CardDefault title='Tilføj batteri'>
           <Grid item xs={12}>
-            <h1>1</h1>
+            <QrReader
+              onScan={this.handleCameraScan}
+              onError={this.handleCameraError} />
+            <h3>{this.state.result}</h3>
           </Grid>
         </CardDefault>
         <Navigation />
