f628cceb3c6d4d0c7fa19380b31160763a9aaff0
Jacob Kostecki <kostecki.jacob@gmail.com>
2018-06-18 21:49:30 +0200

    BatteriesLastSeen

---
+++ src/components/Statistics/BatteriesLastSeen/BatteriesLastSeen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+
+import styles from './BatteriesLastSeen.module.css';
+
+const BatteriesLastSeen = (props) => {
+  return (
+    <Card className={styles.BatteriesLastSeen}>
+      <p>Batterier (Sidst set)</p>
+      <div className={styles.elementsContainer}>
+        {props.data.map((item, i) => <h4>{item.name}: <span className={styles.volt}>{item.voltage} Volt</span></h4>
+      )}
+      </div>
+    </Card>
+  )
+}
+
+export default BatteriesLastSeen;
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -4,7 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import CardDefault from './../UI/CardDefault/CardDefault';
 import AvgSpeed from './AvgSpeed/AvgSpeed';
 import BatteryVoltage from './BatteryVoltage/BatteryVoltage';
-import BattMinmax from './BattMinMax/BattMinMax';
+import BatteriesLastSeen from './BatteriesLastSeen/BatteriesLastSeen';
 import UsageInterval from './UsageInterval/UsageInterval';
 import DailyBarGraph from './DailyBarGraph/DailyBarGraph';
 import VoltageGraph from './VoltageGraph/VoltageGraph';
@@ -12,14 +12,29 @@ import Navigation from '../Navigation/Navigation';
 
 import styles from './Statistics.module.css';
 
+let lastSeenData = [
+  {
+    name: 'ThunderDucks',
+    voltage: 11.7
+  },
+  {
+    name: 'LightningGeese',
+    voltage: 12.3
+  },
+  {
+    name: 'EnergySwallows',
+    voltage: 12.7
+  }
+]
+
 const Statistics = () => (
   <React.Fragment>
     <div className={styles.statsContainer}>
       <CardDefault title='Statistik'>
         <Grid item xs={12} className={styles.group1}>
           <BatteryVoltage voltage={'12,7'} />
-          <BattMinmax minVolt={11.0} maxVolt={13.1} />
-          <AvgSpeed download={'314 kbit/s'} upload={'129 kbit/s'} />
+          <BatteriesLastSeen data={lastSeenData} />
+          <AvgSpeed download={'314 Kbit/s'} upload={'129 Kbit/s'} />
         </Grid>
         <Grid item xs={12} className={styles.group2}>
           <UsageInterval period={'Time'} upload={0.5} download={2} />
