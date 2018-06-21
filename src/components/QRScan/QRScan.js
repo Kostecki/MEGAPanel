5bc19eada3b3a1ac4f2394efdcd5b8ca0feb9f44
Jacob Kostecki <kostecki.jacob@gmail.com>
2018-06-21 14:56:45 +0200

    Post QR result to API

--- src/components/QRScan/QRScan.js
+++ src/components/QRScan/QRScan.js
@@ -1,41 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import QrReader from 'react-qr-reader'
 import Grid from '@material-ui/core/Grid';
 import Swal from 'sweetalert2';
 
+import Axios from '../../utils/api';
 import CardDefault from '../UI/CardDefault/CardDefault';
 import Navigation from '../Navigation/Navigation';
 
-class QRScan extends Component {
-  state = {
-    batteryId: ''
-  }
-
-  handleCameraScan = (data) => {
+const QRScan = () => {
+  const handleCameraScan = (data) => {
     if (data !== this.state.batteryId && data !== null) {
-      this.setState({batteryId: data}, () => Swal('Awesome!', `${this.state.batteryId} has been added`, 'success'))
-      console.log("Posting", this.state.batteryId);
+      Axios.post('/changebattery', {
+        batId: data
+      })
+        .then((response) => {
+          Swal(response, `${data} has been added`, 'success')
+        })
+        .catch((error) => {
+          console.log(error);
+        })
     }
   }
 
-  handleCameraError = (err) => {
+  const handleCameraError = (err) => {
     console.log(err);
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <CardDefault title='Tilføj batteri'>
-          <Grid item xs={12}>
-            <QrReader
-              onScan={this.handleCameraScan}
-              onError={this.handleCameraError} />
-          </Grid>
-        </CardDefault>
-        <Navigation />
-      </React.Fragment>
-    )
-  }
+  return (
+    <React.Fragment>
+      <CardDefault title='Tilføj batteri'>
+        <Grid item xs={12}>
+          <QrReader
+            onScan={handleCameraScan}
+            onError={handleCameraError} />
+        </Grid>
+      </CardDefault>
+      <Navigation />
+    </React.Fragment>
+  )
 }
 
 export default QRScan;
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
-import auth from '../utils/auth.js';
 
 const Axios = axios.create({
   baseURL: 'https://megalight.azurewebsites.net',
   auth: {
-    username: process.env.API_USER,
-    password: process.env.API_PASS
+    username: process.env.REACT_APP_API_USER,
+    password: process.env.REACT_APP_API_PASS
   }
 });
 
