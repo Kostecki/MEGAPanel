f10e2351b26fe711fcb7f36ebdbe3b274c8cd4ae
Jacob Kostecki <kostecki.jacob@gmail.com>
2018-06-21 15:53:28 +0200

    approx voltage level in %

--- src/components/Statistics/BatteryVoltage/BatteryVoltage.js
+++ src/components/Statistics/BatteryVoltage/BatteryVoltage.js
@@ -24,13 +24,52 @@ const BatteryVoltage = (props) => {
 
     return [health, icon];
   }
+
+  const chargeLevel = () => {
+    let voltage = props.data.Voltage;
+
+    if (voltage >= 12.73) {
+      return "100";
+    }
+    if (voltage >= 12.62) {
+      return "90";
+    } 
+    if (voltage >= 12.5) {
+      return "80";
+    }
+    if (voltage >= 12.37) {
+      return "70";
+    }
+    if (voltage >= 12.24) {
+      return "60";
+    }
+    if (voltage >= 12.10) {
+      return "50";
+    }
+    if (voltage >= 11.96) {
+      return "40";
+    }
+    if (voltage >= 11.81) {
+      return "30";
+    }
+    if (voltage >= 11.66) {
+      return "20";
+    }
+    if (voltage >= 11.51) {
+      return "10";
+    }
+    if (voltage < 11.51) {
+      return "0";
+    }
+  }
   
 
   return (
     <Card className={styles.voltage}>
       <p>Batteri</p>
       <div className={`${styles.voltageStatus} ${chargeStatus()[0]}`}>
-        <h2>{props.data.Voltage} Volt</h2><Icon>{chargeStatus()[1]}</Icon>
+        <h2>{props.data.Voltage} Volt </h2>
+        <h2>{chargeLevel()} %</h2>
       </div>
     </Card>
   )
