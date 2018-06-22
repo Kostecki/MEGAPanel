4be3f81f5ea5db390aac6ce230d5af4096a6daea
Jacob Kostecki <kostecki.jacob@gmail.com>
2018-06-22 09:58:58 +0200

    rename card and add units

--- src/components/Statistics/AvgSpeed/AvgSpeed.js
+++ src/components/Statistics/AvgSpeed/AvgSpeed.js
@@ -5,9 +5,9 @@ import styles from './AvgSpeed.module.css';
 
 const AvgSpeed = (props) => (
   <Card className={styles.avgSpeed}>
-    <p>Avg. Speeds</p>
-    <h4>Download: <span className={styles.speed}>{props.download}</span></h4>
-    <h4>Upload: <span className={styles.speed}>{props.upload}</span></h4>
+    <p>Gennemsnitshastighed</p>
+    <h4>Download: <span className={styles.speed}>{props.download} kbit/s</span></h4>
+    <h4>Upload: <span className={styles.speed}>{props.upload} kbit/s</span></h4>
   </Card>
 )
 
