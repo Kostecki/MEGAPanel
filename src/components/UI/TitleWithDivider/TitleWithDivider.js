e2a6ada325b8ffc6b7a2a44d22ff080099e4c641
Jacob Kostecki <kostecki.jacob@gmail.com>
2018-08-09 11:12:03 +0200

    add animation to refresh button

--- src/components/UI/TitleWithDivider/TitleWithDivider.js
+++ src/components/UI/TitleWithDivider/TitleWithDivider.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Divider from '@material-ui/core/Divider';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSyncAlt } from '@fortawesome/free-solid-svg-icons'
 
 import styles from './TitleWithDivider.module.css';
 
-const TitleWithDivider = (props) => {
-  let showRefresh = props.showRefresh;
+class TitleWithDivider extends Component {
+  state = {
+    animate: false
+  }
 
-  return (
-    <React.Fragment>
-      <h3 className={styles.cardTitle}>{props.title} {showRefresh > 0 && <FontAwesomeIcon onClick={props.clickAction} className={styles.refresh} icon={faSyncAlt} />}</h3>
+  showRefresh = this.props.showRefresh;
+
+  render() {
+    return (
+      <React.Fragment>
+      <h3 className={styles.cardTitle}>{this.props.title} {this.showRefresh > 0 && <FontAwesomeIcon onTransitionEnd={() => this.setState({animate: false})}
+                                                                                                    onClick={(event) => { this.props.clickAction(); this.setState({animate: true}) }}
+                                                                                                    className={`${styles.refresh} ${this.state.animate ? 'animate' : null }`}
+                                                                                                    icon={faSyncAlt} />}</h3>
       <Divider className={styles.divider} />
     </React.Fragment>
-  )
+    )
+  }
 }
 
 export default TitleWithDivider;
