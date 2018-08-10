18f644f3a20af37b0d9b6645a8a6a751bc994dd2
Jacob Kostecki <kostecki.jacob@gmail.com>
2018-08-10 14:00:55 +0200

    OTAUpload

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 
 import CardDefault from '../UI/CardDefault/CardDefault';
 import Navigation from '../Navigation/Navigation';
@@ -12,8 +13,12 @@ const Admin = () => {
         <React.Fragment>
             <CardDefault title="Admin">
                 <div className={styles.container}>
-                    <Login />
-                    <OTAUpload />
+                    <Grid item xs={12}>
+                        <OTAUpload />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Login />
+                    </Grid>
                 </div>
             </CardDefault>
             <Navigation />
--- src/components/Admin/OTAUpload/OTAUpload.js
+++ src/components/Admin/OTAUpload/OTAUpload.js
@@ -1,28 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 
+import axios from '../../../utils/api';
+
 import styles from './OTAUpload.module.css';
 
-const OTAUpload = () => {
-    return (
-        <div className={styles.container}>
-            <form className={styles.form}>
-                <Input
-                    className={styles.input}
-                    type="file"
-                />
-                <div className={styles.buttons}>
-                    <Button type="submit" variant="contained" color="primary">
-                        Flash Firmware
+class OTAUpload extends Component {
+    state = {
+        file: null,
+        uploadStatus: 'Upload en fil..'
+    }
+
+    onChange = (e) => {
+        this.setState({ file: e.target.files[0] });
+    }
+
+    handleUploadFW = (e) => {
+        e.preventDefault();
+
+        const data = new FormData();
+        data.append('file', this.state.file);
+
+        axios.post('/upload', data) //This needs an actual endpoint
+        .then((response) => {
+            console.log(response); //Update status
+        })
+        .catch((error) => {
+            console.log(error); //Update status
+        });
+    }
+
+    render() {
+        return (
+            <div className={styles.container}>
+                <p className={styles.p}>Status: <span className={styles.status}>{this.state.uploadStatus}</span></p>
+                <p className={styles.p}>Nuværende FW Version: </p> {/* Fetch from API */}
+                
+                <form className={styles.form} onSubmit={this.handleUploadFW}>
+                    <Input
+                        className={styles.input}
+                        onChange={this.onChange}
+                        type="file"
+                    />
+                    <div className={styles.buttons}>
+                        <Button type="submit" variant="contained" color="primary">
+                            Flash Firmware
                     </Button>
-                    <Button type="submit" variant="contained" color="secondary">
-                        Flash tidligere FW
+                        <Button type="submit" variant="contained" color="secondary">
+                            Flash tidligere FW
                     </Button>
-                </div>
-            </form>
-        </div>
-    )
+                    </div>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default OTAUpload;
