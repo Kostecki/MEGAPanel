f1d3227fb13ce9818bb0318bdd72bfe754234748
Jacob Kostecki <kostecki.jacob@gmail.com>
2018-08-14 12:50:46 +0200

    add animation speed slider

--- src/components/Lights/Animations/Animations.js
+++ src/components/Lights/Animations/Animations.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
+import Slider from '@material-ui/lab/Slider';
 
 import Animation from './Animation/Animation';
 
@@ -7,25 +8,56 @@ import styles from './Animations.module.css';
 
 class Animations extends Component {
   state = {
-    selected: this.props.activeAnimation
+    selected: this.props.currentSelection.Animation,
+    speed: 30
   }
 
-  toggleSelected = (selected) => {
-    this.setState(prevState => ({selected: prevState.selected === selected ? '' : selected}))
+  handleSpeedChange = (event, speed) => {
+    this.setState({ speed }, () => {
+      this.props.animationSpeedChange(speed);
+    });
   };
 
-  animationTypes = ['bpm', 'candy cane', 'confetti', 'cyclon rainbow', 'dots', 'fire', 'glitter', 'juggle', 'lightning', 'police all', 'police one', 'rainbow', 'ripple', 'rainbow with glitter', 'sinelon', 'twinkle', 'noise'];
+  toggleSelected = (selected) => {
+    this.setState(prevState => ({ selected: prevState.selected === selected ? null : selected }))
+  };
+
+  animationTypes = ['bpm',
+    'candy cane',
+    'confetti',
+    'cyclon rainbow',
+    'dots',
+    'fire',
+    'glitter',
+    'juggle',
+    'lightning',
+    'police all',
+    'police one',
+    'rainbow',
+    'ripple',
+    'rainbow with glitter',
+    'sinelon',
+    'twinkle',
+    'noise'];
 
   render() {
     return (
-      <Grid item xs={12} className={styles.animations}>
-        { this.animationTypes.map((item, i) => <Animation key={i}
-                                                id={i}
-                                                animation={item}
-                                                toggleSelected={this.toggleSelected}
-                                                active={this.state.selected === item}
-                                                animationClickHandler={this.props.animationClickHandler}/>) }
-      </Grid>
+      <React.Fragment>
+        <Grid item xs={12} sm={4}>
+          <div className={styles.speed}>
+            <p className={styles.p} onClick={() => {this.setState({speed: 30})}}>Animationshastighed ({this.state.speed})</p>
+            <Slider style={{padding: 0}} value={this.state.speed} min={10} max={200} step={10} onChange={this.handleSpeedChange} />
+          </div>
+        </Grid>
+        <Grid item xs={12} className={styles.animations}>
+          {this.animationTypes.map((item, i) => <Animation key={i}
+            id={i}
+            animation={item}
+            toggleSelected={this.toggleSelected}
+            active={this.state.selected === item}
+            animationClick={this.props.animationClick} />)}
+        </Grid>
+      </React.Fragment>
     )
   }
 }
