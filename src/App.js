14883043b5d96b0f9778aaf7fa4e5d02407f3342
Jacob Kostecki <kostecki.jacob@gmail.com>
2018-08-20 11:14:33 +0200

    toast bar status for ping/pong

--- src/App.js
+++ src/App.js
@@ -4,23 +4,42 @@ import ReactGA from 'react-ga';
 
 import withAuthentication from './components/Auth/withAuthentication';
 
+import Axios from './utils/api';
 import Lights from './components/Lights/Lights';
 import Stats from './components/Statistics/Statistics';
 import QRScan from './components/QRScan/QRScan';
 import Admin from './components/Admin/Admin';
 import Login from './components/Auth/Login/Login';
+import Toast from './components/UI/Toast/Toast';
 
 import './App.css';
 
 ReactGA.initialize('UA-39781810-11');
 
 class App extends Component {
-  componentDidMount = () => ReactGA.pageview(window.location.pathname + window.location.search);
+  componentDidMount = () => {
+    ReactGA.pageview(window.location.pathname + window.location.search)
+
+    this.fetchFromApi();
+  };
   componentDidUpdate = () => ReactGA.pageview(window.location.pathname + window.location.search);
+
+  fetchFromApi = () => {
+    Axios.get('/ping')
+      .then((response) => {
+        if (response.data.response !== "pong") {
+          this.visible = true; //Show danger toast if controller doesn't response to our ping/pong
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
 
   render() {
     return (
       <div className="App">
+        {this.visible && ( <Toast type="success" text="Oops.. Controlleren svarer ikke. Den er nok offline!" /> )}
         <Switch>
           <Route exact path='/' component={Stats} />
           <Route path='/lights' component={Lights} />
