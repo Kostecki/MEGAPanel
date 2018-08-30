4c7a249ab190afb96c50a95ce6ed73b039b90858
Jacob Kostecki <kostecki.jacob@gmail.com>
2018-08-30 12:47:53 +0200

    force upgrade rancher with new image

---
+++ rancher_upgrade.py
@@ -0,0 +1,26 @@
+import json
+import requests
+
+url = "https://rancher.mega.re/v3/projects/$RANCHER_PROJECT/workloads/$RANCHER_WORKLOAD"
+auth_token = "$RANCHER_TOKEN"
+headers = {"Authorization": "Bearer " + auth_token}
+
+getResponse = requests.get(url, headers=headers)
+output = json.loads(getResponse.content)
+container = output["containers"]
+
+jsonobject = {"containers": [{}]}
+jsonobject["containers"] = container
+
+print("Was: ${}".format(jsonobject["containers"][0]["image"]))
+if jsonobject["containers"][0]["image"] == "kostecki/megapanel":
+    jsonobject["containers"][0]["image"] = "kostecki/megapanel:latest"
+else:
+    jsonobject["containers"][0]["image"] = "kostecki/megapanel"
+print("Is: ${}".format(jsonobject["containers"][0]["image"]))
+
+contentType = {"content-type": "application/json"}
+headers.update(**contentType)
+
+putResponse = requests.put(url, data=json.dumps(jsonobject), headers=headers)
+print(putResponse.content)
