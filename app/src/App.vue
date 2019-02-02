614abdb34c063b086e5d62b80507ad7513313585
Jacob Kostecki <kostecki.jacob@gmail.com>
2019-02-02 15:56:58 +0100

    add navigation component

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,19 @@
 <template>
+  <v-app>
+    <Navigation />
+    <main>
+      <router-view></router-view>
+    </main>
+  </v-app>
 </template>
+
+<script>
+import Navigation from './components/Navigation/Navigation.vue'
+
+export default {
+  components: { Navigation }
+}
+</script>
 
 <style lang="scss">
 </style>
---
+++ app/src/components/Navigation/Navigation.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card flat>
+    <v-bottom-nav :active.sync="bottomNav" :value="true" fixed color="white">
+      <v-btn
+        v-for="(item, index) in menuItems"
+        :key="index"
+        color="#FF3D02"
+        flat
+        :value="item.value"
+        router
+        :to="item.link"
+      >
+        <span>{{ item.title }}</span>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-btn>
+    </v-bottom-nav>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        bottomNav: 'recent',
+        menuItems: [
+          { icon: 'color_lens', title: 'Lights', value: 'lights', link: '/lights' },
+          { icon: 'bar_chart', title: 'Statistics', value: 'statistics', link: '/statistics' },
+          { icon: 'settings', title: 'Settings', value: 'settings', link: '/settings' }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
