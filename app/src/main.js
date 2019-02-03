84323e3bc509101602cd20501d7c4c9f609d736c
Jacob Kostecki <kostecki.jacob@gmail.com>
2019-02-04 00:12:34 +0100

    user auth with firebase

--- app/src/main.js
+++ app/src/main.js
@@ -2,13 +2,20 @@ import Vue from 'vue'
 
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import * as firebase from 'firebase'
 
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import Alert from './components/Shared/Alert.vue'
+
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+// Custom global alert component
+Vue.component('AppAlert', Alert)
 
 // Vuetify
 Vue.use(Vuetify)
@@ -16,5 +23,14 @@ Vue.use(Vuetify)
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAio1DTLRtg4x0J9M9yRq3DTQK688Z6toc',
+      authDomain: 'megapanel-3439e.firebaseapp.com',
+      databaseURL: 'https://megapanel-3439e.firebaseio.com',
+      projectId: 'megapanel-3439e',
+      storageBucket: 'megapanel-3439e.appspot.com'
+    })
+  }
 }).$mount('#app')
