c8c2ae37df303091abe304c23606f48698412d5b
Jacob Kostecki <kostecki.jacob@gmail.com>
2019-02-24 00:30:32 +0100

    statistics view

---
+++ app/src/components/Statistics/Statistics.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 align-center>
+        <v-card color="white">
+          <v-card-title>
+            <span class="title font-weight-light text-uppercase">Statistics</span>
+            <v-spacer></v-spacer>
+            <v-icon right medium class="refresh">
+              refresh
+            </v-icon>
+          </v-card-title>
+
+          <v-divider />
+
+          <v-container grid-list-md>
+            <v-layout row class="mb-2">
+              <v-flex xs12 sm4>
+                <CurrentBattery />
+              </v-flex>
+              <v-flex xs12 sm4>
+                <Batteries />
+              </v-flex>
+              <v-flex xs12 sm4>
+                <AvgSpeeds />
+              </v-flex>
+            </v-layout>
+
+            <v-divider />
+
+            <v-layout row class="my-2">
+              <v-flex xs12>
+                <DataUsageGraphs />
+              </v-flex>
+            </v-layout>
+
+            <v-layout row class="my-2">
+              <v-flex xs12>
+                <DataUsageToday />
+              </v-flex>
+            </v-layout>
+
+            <v-layout row class="mt-2">
+              <v-flex xs12>
+                <WeeklyBatteryData />
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+  
+</template>
+
+<script>
+import CurrentBattery from './CurrentBattery.vue'
+import Batteries from './Batteries.vue'
+import AvgSpeeds from './AvgSpeeds.vue'
+import DataUsageGraphs from './DataUsageGraphs.vue'
+import DataUsageToday from './DataUsageToday.vue'
+import WeeklyBatteryData from './WeeklyBatteryData.vue'
+
+export default {
+  components: {
+    CurrentBattery,
+    Batteries,
+    AvgSpeeds,
+    DataUsageGraphs,
+    DataUsageToday,
+    WeeklyBatteryData
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+<style lang="scss" scoped>
+
+</style>
