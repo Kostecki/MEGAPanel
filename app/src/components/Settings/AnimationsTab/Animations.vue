5b25cc3710ded95e6b465a6386f61f732654119a
Jacob Kostecki <kostecki.jacob@gmail.com>
2019-03-01 18:43:41 +0100

    catch no animations

--- app/src/components/Settings/AnimationsTab/Animations.vue
+++ app/src/components/Settings/AnimationsTab/Animations.vue
@@ -1,49 +1,58 @@
 <template>
   <div class="animations">
-    <div v-if="animationsClone.length === 0" class="text-xs-center" style="padding-bottom: 24px">
-      <h4>No Animations</h4>
+    <div v-if="!animationsClone" class="text-xs-center">
+      <v-progress-circular
+        indeterminate
+        color="primary"
+        class="mb-4"
+      ></v-progress-circular>
     </div>
-    <div v-else v-for="(animation, index) in animationsClone" :key="index" class="animation">
-      <v-layout align-center row class="create-new">
-        <v-flex xs11>
-          <v-layout align-end justify-space-between row class="create-new pa-0">
-            <v-flex xs4>
-              <v-text-field
-                v-model="animationsClone[index]['name']"
-                label="Name"
-                @change="updateAnimation(animation.key, index)"
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs4>
-              <v-text-field
-                v-model="animationsClone[index]['value']"
-                label="Value"
-                @change="updateAnimation(animation.key, index)"
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs3>
-              <v-checkbox
-                v-model="animation.speedControl"
-                label="Speed Control"
-                color="primary"
-                @change="updateAnimation(animation.key, index)"
-              ></v-checkbox>
-            </v-flex>
-          </v-layout>
-        </v-flex>
-        <v-flex xs1>
-          <v-layout justify-end>
-            <v-btn
-              flat icon
-              color="grey"
-              @click="triggerDeleteConfirm(animation.key)"
-              style="margin: 0"
-            >
-              <v-icon>remove_circle_outline</v-icon>
-            </v-btn>
-          </v-layout>
-        </v-flex>
-      </v-layout>
+    <div v-else>
+      <div v-if="animationsClone && animationsClone.length === 0" class="text-xs-center" style="padding-bottom: 24px">
+        <h4>No Animations</h4>
+      </div>
+      <div v-else v-for="(animation, index) in animationsClone" :key="index" class="animation">
+        <v-layout align-center row class="create-new">
+          <v-flex xs11>
+            <v-layout align-end justify-space-between row class="create-new pa-0">
+              <v-flex xs4>
+                <v-text-field
+                  v-model="animationsClone[index]['name']"
+                  label="Name"
+                  @change="updateAnimation(animation.key, index)"
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs4>
+                <v-text-field
+                  v-model="animationsClone[index]['value']"
+                  label="Value"
+                  @change="updateAnimation(animation.key, index)"
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs3>
+                <v-checkbox
+                  v-model="animation.speedControl"
+                  label="Speed Control"
+                  color="primary"
+                  @change="updateAnimation(animation.key, index)"
+                ></v-checkbox>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex xs1>
+            <v-layout justify-end>
+              <v-btn
+                flat icon
+                color="grey"
+                @click="triggerDeleteConfirm(animation.key)"
+                style="margin: 0"
+              >
+                <v-icon>remove_circle_outline</v-icon>
+              </v-btn>
+            </v-layout>
+          </v-flex>
+        </v-layout>
+      </div>
     </div>
 
     <v-dialog v-model="dialog" max-width="290">
